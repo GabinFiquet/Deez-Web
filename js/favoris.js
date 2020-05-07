ffe534dbcb0940fbb14ef5844f10494b26166f00
GabinFiquet <gabin.fiquet@gmail.com>
2020-05-07 10:49:46 +0200

    Ajout d'un texte en cas d'absence de favoris sur la page favoris et retrait du switch case et modification des value des option de tri pour la page recherche

--- js/favoris.js
+++ js/favoris.js
@@ -1,30 +1,32 @@
 $(function() {
     let storageJSON = localStorage.getItem("DeezWebFavoris");
         favorisList = storageJSON ? JSON.parse(storageJSON) : storageJSON;
+    if (favorisList.length != 0){
+        for (let i=0; i< favorisList.length; i++){
+            $('.songFavList').append(`
+                        <article class="flex-row card" id="card${i}">
+                            <div class="flex-col description" id="description${i}">
+                                <p><span class="title" id="title${i}">${favorisList[i].title}</span> - <span class="artist" id="artist${i}">${favorisList[i].artist}</span></p>
+                                <p class="album" id="album${i}">${favorisList[i].album}</p>
+                                <button id="buttonFav${i}" class="btn-DzW removeFav">Retire des favoris</button>
+                            </div>
+                            <img class="cover" id="cover${i}" src="${favorisList[i].cover}">
+                            <audio controls class="player" id="player${i}" src="${favorisList[i].player}"></audio>
+                        </article>`);
 
-    for (let i=0; i< favorisList.length; i++){
-        $('.songFavList').append(`
-                    <article class="flex-row card" id="card${i}">
-                        <div class="flex-col description" id="description${i}">
-                            <p><span class="title" id="title${i}">${favorisList[i].title}</span> - <span class="artist" id="artist${i}">${favorisList[i].artist}</span></p>
-                            <p class="album" id="album${i}">${favorisList[i].album}</p>
-                            <button id="buttonFav${i}" class="btn-DzW removeFav">Retire des favoris</button>
-                        </div>
-                        <img class="cover" id="cover${i}" src="${favorisList[i].cover}">
-                        <audio controls class="player" id="player${i}" src="${favorisList[i].player}"></audio>
-                    </article>`);
+            let idFavoris = favorisList[i].id;
 
-        let idFavoris = favorisList[i].id;
+            $(`#buttonFav${i}`).on('click', idFavoris, function(event){
+                favorisList = favorisList.filter(idFavoris => idFavoris.id !== event.data);
+                localStorage.setItem("DeezWebFavoris", JSON.stringify(favorisList));
 
-        $(`#buttonFav${i}`).on('click', idFavoris, function(event){
-            favorisList = favorisList.filter(idFavoris => idFavoris.id !== event.data);
-            localStorage.setItem("DeezWebFavoris", JSON.stringify(favorisList));
-
-            $(`#card${i}`).addClass('bounceOut');
-            setTimeout(function() {
-                $(`#card${i}`).remove()
-            }, 1000);
-        });
-    };
-    
+                $(`#card${i}`).addClass('bounceOut');
+                setTimeout(function() {
+                    $(`#card${i}`).remove()
+                }, 1000);
+            });
+        };
+    }else{
+        $('.songFavList').append(`<h4>Désolé, tu n'as pas encore de favoris</h4>`);
+    }
 });
